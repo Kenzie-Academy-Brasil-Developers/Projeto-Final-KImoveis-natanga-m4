e1a984ed03448c4b3df640f67b29cb8d2cddc797
natabgaf <natanfernandes52@gmail.com>
2023-05-05 17:55:10 -0300

    feat /completdget

--- src/routers/user.routes.ts
+++ src/routers/user.routes.ts
@@ -3,11 +3,13 @@ import { userSchemaRequests } from "../schemas/user.schema";
 import { createUserControler, getAllUserControler } from './../controllers/users.controllers';
 import { validBodyMiddleware } from './../middlewares/validBody.Middleware';
 import { verifyEmailExistMiddleware } from './../middlewares/verifyEmailExist.Middleware';
+import { ensureTokenIsValidMiddleware } from './../middlewares/ensureTokenIsValid.Middleware';
+import { isAdminMiddleware } from './../middlewares/isAdmin.Middleware';
 
 const userRoutes: Router = Router()
 
 userRoutes.post('', verifyEmailExistMiddleware, validBodyMiddleware(userSchemaRequests), createUserControler);
-userRoutes.get('', getAllUserControler);
+userRoutes.get('', ensureTokenIsValidMiddleware, isAdminMiddleware, getAllUserControler);
 
 
 

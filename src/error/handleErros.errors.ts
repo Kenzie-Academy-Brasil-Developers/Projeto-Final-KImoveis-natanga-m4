b7216dc051532498c9e89764694a57a0808f0120
natabgaf <natanfernandes52@gmail.com>
2023-05-05 12:26:04 -0300

    feat handleErros completd

---
+++ src/error/handleErros.errors.ts
@@ -0,0 +1,27 @@
+import { NextFunction, Request, Response } from "express"
+import { ZodError } from "zod"
+export class AppError extends Error {
+    statusCode: number
+    constructor(message: string, statusCode: number = 400) {
+        super(message)
+        this.statusCode = statusCode
+    }
+}
+
+export const handleErros = (err: Error, req: Request, res: Response, next: NextFunction) => {
+
+    if (err instanceof AppError) {
+        return res.status(err.statusCode).json(err.message)
+    }
+
+    if (err instanceof ZodError) {
+        return res.status(400).json(err.flatten().fieldErrors)
+    }
+
+    console.error(err)
+
+    return res.status(500).json({
+        message: "Internal Server Error."
+    });
+
+}

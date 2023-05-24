b61d04700e9c0249e5f73e3b7d748fccf176c493
natabgaf <natanfernandes52@gmail.com>
2023-05-24 12:07:30 -0300

    deploy

--- src/app.ts
+++ src/app.ts
@@ -10,7 +10,12 @@ import { handleErros } from './error/handleErros.errors'
 
 const app = express()
 app.use(express.json())
-
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  next();
+});
 app.use('/users',userRoutes);
 app.use('/login',loginRoutes);
 app.use('/realEstate', realEstateRoutes);
